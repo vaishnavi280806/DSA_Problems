ecccad2b2ab6254660e71aa96043f45571b1a5cf
vaishnavi280806 <vaishnavi280806@gmail.com>
2025-12-18 00:31:14 +0530

    Time: 7 ms (73.63%) | Memory: 22.9 MB (76.28%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,20 +1,20 @@
 class Solution {
-    int time(vector<int>& piles, int k){
-        int hours = 0;
+    long long time(vector<int>& piles, int k){
+        long long hours = 0;
         for (int i = 0; i < piles.size(); i++){
-            hours += (piles[i] + k - 1 )/ k;
+            hours += ((long long)piles[i] + k - 1 )/k;
         }
         return hours;
     }
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1, high = *max_element(piles.begin(), piles.end());
-        while (low < high){
+        while (low <= high){
             int mid = low + (high - low)/2;
-            int hours = time(piles, mid);
-            if (hours > h) low = mid + 1;
-            else high = mid;
+            long long hours = time(piles, mid);
+            if (hours <= h) high = mid - 1;
+            else low = mid + 1;
         }
-        return high;
+        return low;
     }
 };
