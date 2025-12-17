e01de39ff3f9910b87077fca28cec27ebffdbd8e
vaishnavi280806 <vaishnavi280806@gmail.com>
2025-12-17 23:14:41 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.53%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         if (nums.size() == 1) return nums[0];
         if (nums[0] != nums[1]) return nums[0];
         if (nums[nums.size() - 1] != nums[nums.size() - 2]) return nums[nums.size() - 1];
-        int low = 0, high = nums.size() - 1;
+        int low = 1, high = nums.size() - 2;
         while (low <= high){
             int mid = low + (high - low)/2;
             if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]){
