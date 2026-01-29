4996a9c0439eda2f4a836dca97d34a8d9a5c9b2f
Vaishnavi S <vaishnavi280806@gmail.com>
2026-01-29 22:49:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.cpp
@@ -0,0 +1,18 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    string preToPost(string s) {
+        reverse(s.begin(), s.end());
+        stack <string> stk;
+        for (char x : s){
+            if (isalnum(x)) stk.push(string(1,x));
+            else{
+                string tp1 = stk.top(); stk.pop();
+                string tp2 = stk.top(); stk.pop();
+                stk.push(tp1 + tp2 + x);
+            }
+        }
+        return stk.top();
+    }
+};
