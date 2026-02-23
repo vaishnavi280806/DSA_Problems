4e441e796bcc0082b3d58d442b8d7d42f9f6b151
Vaishnavi S <vaishnavi280806@gmail.com>
2026-02-23 18:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (27.17%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mini = 0, maxi = 0;
+        for (char x : s){
+            if (x == '('){
+                mini += 1;
+                maxi += 1;
+            }
+            else if (x == ')'){
+                mini -= 1;
+                maxi -= 1;
+            }
+            else{
+                mini -= 1;
+                maxi += 1;
+            }
+            if (mini < 0) mini = 0;
+            if (maxi < 0) return false;
+        }
+        return (mini == 0);
+    }
+};
