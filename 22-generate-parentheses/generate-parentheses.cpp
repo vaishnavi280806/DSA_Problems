2cc239fc4dd963f89dde8f33614502fb68f61c8b
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-06 16:55:40 +0530

    Time: 2 ms (81.38%) | Memory: 13.1 MB (82.61%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,31 +1,25 @@
 class Solution {
-    vector<char> para = {'(', ')'};
-    bool valid(string ds){
-        int sum = 0;
-        for (int i = 0; i < ds.size(); i++){
-            if (ds[i] == '(') sum += 1;
-            else sum -= 1;
-            if (sum < 0) return false;
-        }
-        if (sum == 0) return true;
-        return false;
-    }
-    void helper(string& ds, vector<string>& res, int n){
+    void helper(int open, int close, string& ds, vector<string>& res, int n){
         if (ds.size() == 2*n){
-            if (valid(ds)) res.push_back(ds);
+            res.push_back(ds);
             return;
         }
-        for (int i = 0; i < 2; i++){
-            ds.push_back(para[i]);
-            helper(ds, res, n);
+        if (open < n){
+            ds.push_back('(');
+            helper(open + 1, close, ds, res, n);
+            ds.pop_back();
+        }
+        if (close < open){
+            ds.push_back(')');
+            helper(open, close + 1, ds, res, n);
             ds.pop_back();
         }
     }
 public:
     vector<string> generateParenthesis(int n) {
-       vector<string> res;
-       string ds;
-       helper(ds, res, n);
-       return res;
+        vector<string> res;
+        string ds;
+        helper(0, 0, ds, res, n);
+        return res;
     }
 };
