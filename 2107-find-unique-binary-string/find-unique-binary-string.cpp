a322f9928d03c06120876f83dba7cb46862945a9
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-08 23:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (98.60%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,28 +1,11 @@
 class Solution {
-    string helper(string& ds, unordered_set<string>& hash, int n){
-        if (ds.size() == n){
-            if (!hash.count(ds)) return ds;
-            else return "";
-        }
-        ds.push_back('0');
-        string ans = helper(ds, hash, n);
-        if (ans != "") return ans;
-        ds.pop_back();
-        ds.push_back('1');
-        ans = helper(ds, hash, n);
-        if (ans != "") return ans;
-        ds.pop_back();
-
-        return "";
-    }
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set <string> hash;
-        for (auto it : nums){
-            hash.insert(it);
+        string ans = "";
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i][i] == '0') ans += '1';
+            else ans += '0';
         }
-        string ds;
-        return helper(ds, hash, nums.size());
-
+        return ans;
     }
 };
