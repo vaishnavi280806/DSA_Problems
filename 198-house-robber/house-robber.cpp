ada9b9fd12ff1e329d4532bbaf0f8dd1a2f9e600
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-09 23:56:03 +0530

    Time: 1 ms (8.96%) | Memory: 10.7 MB (21.52%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -9,7 +9,15 @@ class Solution {
     }
 public:
     int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
         vector<int> dp(nums.size(), -1);
-        return helper(nums.size() - 1, nums, dp);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++){
+            int way1 = nums[i] + dp[i-2];
+            int way2 = dp[i-1];
+            dp[i] = max(way1, way2);
+        }
+        return dp[nums.size() - 1];
     }
 };
