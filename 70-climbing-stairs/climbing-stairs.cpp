7ff454b1d0d1d6de13900286587cca40200e42c3
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-09 21:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,11 @@
 class Solution {
-    int helper(int n, vector<int>& dp){
-        if (n == 0) return 1;
-        if (n < 0) return 0;
-        if (dp[n] != -1) return dp[n];
-        return dp[n] = helper(n - 1, dp) + helper(n-2, dp);
-    }
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
-        return helper(n, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2;  i <= n; i++) dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
+
     }
 };
