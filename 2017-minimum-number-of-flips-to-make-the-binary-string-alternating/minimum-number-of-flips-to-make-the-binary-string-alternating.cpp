7aa95657f85cac48c409c02863b364fcb9ec3797
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-11 00:43:28 +0530

    Time: 37 ms (36.06%) | Memory: 28.3 MB (27.90%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
     int minFlips(string s) {
-        int n = s.size();
-        s += s;
-        int c = 0;
-        int res = INT_MAX;
-        //odd - 1
-        //even - 0
-        for (int i = 0; i < s.size(); i++){
-            if (i % 2 == 1 && s[i] != '1') c++;
-            else if (i % 2 == 0 && s[i] != '0') c++;
-            if (i >= n && (i-n) % 2 == 1 && s[i-n] != '1' ) c--;
-            else if (i >= n && (i-n) % 2 == 0 && s[i-n] != '0' ) c--;
-            if (i >= n-1) res = min(res, min(c, n-c));
+        int ans = INT_MAX;
+        int a1=0, a2=0, l = 0, n = s.size();
+        s=s+s;
+
+        string s1, s2;
+        int add = 0;
+        for(int i=0;i< s.size();i++){
+            s1+=('0'+add);
+            s2+=('1'-add);
+            add^=1;
         }
-        return res;
+        cout << s1 << " " << s2 << endl;
+
+        for(int i=0;i<n;i++){
+            char b = s[i];
+            if(b!=s1[i]) a1++;
+             if(b!=s2[i]) a2++;
+        }
+        cout << a1 << a2;
+        ans = min(a1, a2);
+        for(int r=n;r<s.size();r++){
+            
+            char b = s[l];
+            if(b!=s1[l]) a1--;
+            if(b!=s2[l]) a2--;
+
+            l++;
+
+            b = s[r];
+            if(b!=s1[r]) a1++;
+            if(b!=s2[r]) a2++;
+            ans = min(ans, a1);
+            ans = min(ans, a2);
+           
+        }
+        
+
+        return ans;
+
     }
 };
