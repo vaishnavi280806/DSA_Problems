3efea55c377525eed88e6e12c84db279b0cd889d
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-12 17:59:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.98%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N = m + n - 2;
+        int r = min(m-1, n-1);
+        long long res = 1;
+        for(int i = 1; i <= r; i++){
+            res = res * (N - r + i)/i;
+        }
+        return res;
+    }
+};
