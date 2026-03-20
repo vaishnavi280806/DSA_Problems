272dc0b312117839e9df94ae6c5fcb2ce500a2b6
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-20 16:20:51 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.67%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -7,13 +7,18 @@ public:
         }
         return nums.size();*/
 
-        vector<int> arr(nums.size()+1, 0);
+        /*vector<int> arr(nums.size()+1, 0);
         for (int i = 0; i < nums.size(); i++){
             arr[nums[i]] = 1;
         }
         for (int i = 0; i < arr.size(); i++){
             if (arr[i] == 0) return i;
         }
-        return -1;
+        return -1;*/
+        int actual = accumulate(nums.begin(), nums.end(), 0);
+        int req = nums.size();
+        req = req * (req+1)/2;
+        return req - actual;
+
     }
 };
