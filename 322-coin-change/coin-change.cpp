301b91626d2bb87176322939f92589141d9a95ff
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-23 19:58:12 +0530

    Time: 47 ms (33.34%) | Memory: 49.1 MB (32.35%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,7 +1,9 @@
 class Solution {
     long long helper(int idx, int amt, vector<int>& coins, vector<vector<int>>& dp){
-        if (amt == 0) return 0;
-        if (idx < 0) return INT_MAX;
+        if (idx == 0){
+            if (amt % coins[0] == 0) return amt / coins[0];
+            return INT_MAX;
+        };
         if (dp[idx][amt] != -1) return dp[idx][amt];
         long long pick = INT_MAX;
         if (coins[idx] <= amt) pick = 1 + helper(idx, amt - coins[idx], coins, dp);
@@ -14,6 +16,20 @@ public:
         int n = coins.size();
         vector<vector<int>> dp (n, vector<int> (amount+1, -1));
         int ans = helper(n-1, amount, coins, dp);
+        /*for (int i = 0; i < n; i++){
+            dp[i][0] = 0;
+        }
+        
+        for (int i = 1; i < n; i++){
+            for (int j = 1; j < amount+1; j++){
+                long long pick = INT_MAX;
+                if (coins[i] <= j) pick = 1 + dp[i][j - coins[i]];
+                long long notpick = INT_MAX;
+                if (i != 0) notpick = dp[i - 1][j];
+
+                return dp[i][j] = min(pick, notpick);
+            }
+        }*/
         if (ans == INT_MAX) return -1;
         return ans;
     }
