d065e395748bbcfc6311490f3aa425f1105a6016
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-23 20:02:55 +0530

    Time: 49 ms (27.09%) | Memory: 48.8 MB (39.22%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    long long helper(int idx, int amt, vector<int>& coins, vector<vector<int>>& dp){
+    /*long long helper(int idx, int amt, vector<int>& coins, vector<vector<int>>& dp){
         if (idx == 0){
             if (amt % coins[0] == 0) return amt / coins[0];
             return INT_MAX;
@@ -10,27 +10,27 @@ class Solution {
         long long notpick = helper(idx - 1, amt, coins, dp);
 
         return dp[idx][amt] = min(pick, notpick);
-    }
+    }*/
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp (n, vector<int> (amount+1, -1));
-        int ans = helper(n-1, amount, coins, dp);
-        /*for (int i = 0; i < n; i++){
-            dp[i][0] = 0;
+        vector<vector<int>> dp (n, vector<int> (amount+1, 0));
+        //int ans = helper(n-1, amount, coins, dp);
+        for (int i = 0; i < amount+1; i++){
+            if (i % coins[0] == 0) dp[0][i] =  i / coins[0];
+            else dp[0][i] = INT_MAX;
         }
         
         for (int i = 1; i < n; i++){
             for (int j = 1; j < amount+1; j++){
                 long long pick = INT_MAX;
-                if (coins[i] <= j) pick = 1 + dp[i][j - coins[i]];
-                long long notpick = INT_MAX;
-                if (i != 0) notpick = dp[i - 1][j];
+                if (coins[i] <= j &&  dp[i][j - coins[i]] != INT_MAX) pick = 1 + dp[i][j - coins[i]];
+                long long notpick = dp[i - 1][j];
 
-                return dp[i][j] = min(pick, notpick);
+                dp[i][j] = min(pick, notpick);
             }
-        }*/
-        if (ans == INT_MAX) return -1;
-        return ans;
+        }
+        if (dp[n-1][amount] == INT_MAX) return -1;
+        return dp[n-1][amount];
     }
 };
