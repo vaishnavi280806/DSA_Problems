80aaf26a09f6bbbc2afd388c91c30dd3765042ff
Vaishnavi S <vaishnavi280806@gmail.com>
2026-03-24 19:20:37 +0530

    Time: 39 ms (34.71%) | Memory: 61.7 MB (9.16%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int helper(int idx, int amt, vector<int>& coins, vector<vector<int>>& dp){
+    /*int helper(int idx, int amt, vector<int>& coins, vector<vector<int>>& dp){
         if (idx == coins.size()){
             if (amt == 0) return 1;
             return 0;
@@ -10,11 +10,22 @@ class Solution {
         int notpick = helper(idx + 1, amt, coins, dp);
 
         return dp[idx][amt] = pick + notpick;
-    }
+    }*/
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return helper(0,  amount, coins, dp);
+        vector<vector<unsigned long long>> dp(n+1, vector<unsigned long long>(amount+1, 0));
+        //return helper(0,  amount, coins, dp);
+        dp[n][0] = 1;
+        for (int idx = n-1; idx >= 0; idx--){
+            for (int amt = 0; amt <= amount; amt++){
+                unsigned long long pick = 0;
+                if (coins[idx] <= amt) pick = dp[idx][amt - coins[idx]];
+                unsigned long long notpick = dp[idx + 1][amt];
+
+                dp[idx][amt] = pick + notpick;
+            }
+        }
+        return dp[0][amount];
     }
 };
