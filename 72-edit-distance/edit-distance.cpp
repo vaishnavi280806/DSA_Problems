a11ce9425d17db0a293b516258b8a4349b2b7d73
Vaishnavi S <vaishnavi280806@gmail.com>
2026-04-11 16:53:46 +0530

    Time: 3 ms (91.94%) | Memory: 13.2 MB (59.03%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int helper(int i, int j, string& word1, string& word2, vector<vector<int>>& dp){
+    /*int helper(int i, int j, string& word1, string& word2, vector<vector<int>>& dp){
         if (i < 0  && j < 0) return 0;
         if (i < 0) return j+1;
         if (j < 0) return i+1;
@@ -12,12 +12,30 @@ class Solution {
 
             return dp[i][j] = min(way1, min(way2, way3));
         }
-    }
+    }*/
 public:
     int minDistance(string word1, string word2) {
         int n1 = word1.size();
         int n2 = word2.size();
-        vector<vector<int>> dp(n1, vector<int> (n2, -1));
-        return helper(n1-1, n2-1, word1, word2, dp);
+        vector<vector<int>> dp(n1+1, vector<int> (n2+1, 0));
+        //return helper(n1-1, n2-1, word1, word2, dp);
+        for (int i = 0; i <= n1; i++){
+            for (int j = 0; j <= n2; j++){
+                if (i == 0 && j == 0) dp[i][j] = 0;
+                else if (i == 0) dp[i][j] = j;
+                else if (j == 0) dp[i][j] = i;
+                else{
+                    if (word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                    else{
+                        int way1 = 1 + dp[i-1][j-1];
+                        int way2 = 1 + dp[i][j-1];
+                        int way3 = 1 + dp[i-1][j];
+
+                        dp[i][j] = min(way1, min(way2, way3));
+                    }
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 };
