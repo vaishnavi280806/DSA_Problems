97a1b227a7f35134734b1d68259dfad21cd32c95
Vaishnavi S <vaishnavi280806@gmail.com>
2026-04-12 22:49:45 +0530

    Time: 555 ms (5.01%) | Memory: 222.6 MB (28.12%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,7 +1,42 @@
 class Solution {
+    /*helper(int idx, int buy, int rem, vector<int>& prices, vector<vector<vector<int>>>& dp){
+        if (idx == prices.size()) return 0;
+        if (rem == 0) return 0;
+        if (dp[idx][buy][rem] != -1) return dp[idx][buy][rem];
+        int profit = 0;
+        if (buy){
+            profit = max(-prices[idx] + helper(idx+1, 0, rem, prices, dp), helper(idx+1, 1, rem, prices, dp));
+        }
+        else{
+            profit = max(prices[idx] + helper(idx+1, 1, rem-1, prices, dp), helper(idx+1, 0, rem, prices, dp));
+        }
+        return dp[idx][buy][rem] = profit;
+    }*/
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
+        //vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, -1)));
+        //return helper(0, 1, 2, prices, dp);
+
+        /*for (int idx = n; idx >= 0; idx--){
+            for (int buy = 0; buy < 2; buy++){
+                for (int rem = 0; rem < 3; rem++){
+                    if (idx == n) dp[idx][buy][rem] = 0;
+                    else if (rem == 0) dp[idx][buy][rem] = 0;
+                    else{
+                        int profit = 0;
+                        if (buy){
+                            profit = max(-prices[idx] + dp[idx+1][0][rem], dp[idx+1][1][rem]);
+                        }
+                        else{
+                            profit = max(prices[idx] + dp[idx+1][1][rem-1], dp[idx+1][0][rem]);
+                        }
+                        dp[idx][buy][rem] = profit;
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];*/
 
         vector<vector<int>> prev(2, vector<int> (3, 0));
         for (int idx = n; idx >= 0; idx--){
