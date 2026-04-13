83de1722eab208bdc572e6370cfdb50308a38cb3
Vaishnavi S <vaishnavi280806@gmail.com>
2026-04-13 22:33:50 +0530

    Time: 257 ms (30.58%) | Memory: 294.8 MB (26.32%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -14,7 +14,7 @@ public:
         //vector<vector<int>> dp(n, vector<int> (n+1, -1e5));
         //return helper(0, -1, nums, dp);
 
-        vector<vector<int>> dp (n+1, vector<int> (n+1, 0));
+        /*vector<vector<int>> dp (n+1, vector<int> (n+1, 0));
         
         for (int idx = n-1; idx >= 0; idx--){
             for (int prev = idx-1; prev >= -1; prev--){
@@ -25,6 +25,22 @@ public:
                 dp[idx][prev+1] = len;
             }
         }
-        return dp[0][0];
+        return dp[0][0];*/
+
+        vector<int> prevA(n+1, 0);
+
+        for (int idx = n-1; idx >= 0; idx--){
+            vector<int> temp(n+1, 0);
+            for (int prev = idx-1; prev >= -1; prev--){
+                int len = prevA[prev+1];
+                if (prev == -1 || nums[idx] > nums[prev]){
+                    len = max(len, 1+ prevA[idx+1]);
+                }
+                temp[prev+1] = len;
+            }
+            prevA = temp;
+        }
+        return prevA[0];
+        
     }
 };
