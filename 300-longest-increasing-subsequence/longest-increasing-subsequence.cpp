1abce865e74f749888df378746a4c498ba5a7306
Vaishnavi S <vaishnavi280806@gmail.com>
2026-04-13 22:43:47 +0530

    Time: 61 ms (57.80%) | Memory: 14.4 MB (57.54%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -43,14 +43,16 @@ public:
         return prevA[0];*/
 
         vector<int> dp(n, 1);
+        int maxi = 1;
         for (int i = 0; i < n; i++){
             for (int j = 0; j < i; j++){
                 if (nums[i] > nums[j]){
                     dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
+            maxi = max(maxi, dp[i]);
         }
-        return *max_element(dp.begin(), dp.end());
+        return maxi;
         
     }
 };
