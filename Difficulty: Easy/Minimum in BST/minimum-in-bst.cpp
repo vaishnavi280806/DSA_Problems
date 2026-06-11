bab16dda447d843a7cfad8e072821a353bb6f57e
Vaishnavi S <vaishnavi280806@gmail.com>
2026-06-11 22:44:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum in BST/minimum-in-bst.cpp
@@ -0,0 +1,22 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        while (root->left) root = root->left;
+        return root->data;
+    }
+};
