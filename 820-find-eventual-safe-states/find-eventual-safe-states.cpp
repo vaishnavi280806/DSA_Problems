487b205f5894babe8a661fb2e2571a1382323d4f
Vaishnavi S <vaishnavi280806@gmail.com>
2026-06-20 00:03:43 +0530

    Time: 49 ms (45.34%) | Memory: 66.2 MB (35.15%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,43 +1,39 @@
 class Solution {
-    bool dfs(int node, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& path, unordered_set<int>& res){
-        visited[node] = true;
-        path[node] = true;
-
-        for (int x : graph[node]){
-            if (!visited[x]){
-                if (dfs(x, graph, visited, path, res)){
-                    return true;
-                }
-            }
-            else{
-                if (path[x]){
-                    return true;
-                }
-            }
-        }
-        res.insert(node);
-        path[node] = false;
-        return false;
-
-    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<bool> visited(n, false);
-        vector<bool> path(n, false);
-        vector<int> ans;
-        unordered_set<int> res;
+        vector<vector<int>> rev(n);
+        vector<int> indegree(n, 0);
 
         for (int i = 0; i < n; i++){
-            if (!visited[i]){
-                if (!dfs(i, graph, visited, path, res)) res.insert(i);
+            for (auto it : graph[i]){
+                rev[it].push_back(i);
+                indegree[i]++;
             }
         }
 
-        for (int i = 0; i < n; i++) if (graph[i].size() == 0) res.insert(i);
-        for (int x : res) ans.push_back(x);
-        sort(ans.begin(), ans.end());
-
-        return ans;
+        vector<int> res;
+        queue<int> q;
+        
+        for (int i = 0; i < n; i++){
+            if (indegree[i] == 0){
+                q.push(i);
+                res.push_back(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            for (int x : rev[node]){
+                if (--indegree[x] == 0){
+                    q.push(x);
+                    res.push_back(x);
+                }
+            }
+        }
+        sort(res.begin(), res.end());
+        return res;;
     }
 };
