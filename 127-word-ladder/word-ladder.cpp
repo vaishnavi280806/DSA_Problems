60c0a031882831b13f4cda27f31c7f5ebeb94675
Vaishnavi S <vaishnavi280806@gmail.com>
2026-06-22 23:28:15 +0530

    Time: 162 ms (31.84%) | Memory: 21.3 MB (32.11%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,62 +1,37 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& words) {
-        int n = words.size();
-        int s = beginWord.size();
-        vector<vector<int>> graph(n);
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> words;
         int flag = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (words[i] == endWord) flag = 1;
-            for (int j = i + 1; j < n; j++) {
-                int k = 0;
-                int c = 0;
-                while (k < s) {
-                    if (words[i][k] != words[j][k]) c++;
-                    if (c > 1) break;
-                    k++;
-                }
-                if (c == 1){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                }
-            }
+        for (auto word : wordList){
+            words.insert(word);
+            if (endWord == word) flag = 1;
         }
+        if (flag == 0) return 0;
 
-        if (words[n-1] != endWord && flag == 0) return 0;
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
 
-        queue<pair<int, int>> q;
-        vector<bool> visited(n, false);
-        for (int i = 0; i < n; i++){
-            int k = 0;
-                int c = 0;
-                while (k < s) {
-                    if (beginWord[k] != words[i][k]) c++;
-                    if (c > 1) break;
-                    k++;
-                }
-                if (c == 1){
-                    q.push({i, 2});
-                    visited[i] = true;
-                }
-        }
-        int ans = INT_MAX;
+        int n = beginWord.size();
         while (!q.empty()){
             auto node = q.front();
+            string word = node.first;
             q.pop();
 
-            if (words[node.first] == endWord){
-                ans = min(ans, node.second);
-                continue;
-            }
+            if (word == endWord) return node.second;
 
-            for (int x : graph[node.first]){
-                if (!visited[x]){
-                    q.push({x, node.second + 1});
-                    visited[x] = true;
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < 26; j++){
+                    string newW = word.substr(0, i) + char(j + 97) + word.substr(i+1, n - i);
+                    //cout << newW << " ";
+                    if (words.count(newW)){
+                        q.push({newW, node.second + 1});
+                        words.erase(newW);
+                    }
                 }
             }
+
         }
-        if (ans == INT_MAX) return 0;
-        return ans;
+        return 0;
     }
 };
