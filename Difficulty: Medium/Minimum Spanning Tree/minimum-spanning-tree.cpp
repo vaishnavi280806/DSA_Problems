21a6c91aaec475495eb79b237e975fb039edcbfc
Vaishnavi S <vaishnavi280806@gmail.com>
2026-06-28 03:08:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,36 +1,55 @@
+class DisjointSet{
+    vector<int> rank, parent;
+    public:
+        DisjointSet(int n){
+            rank.resize(n+1, 0);
+            parent.resize(n+1);
+            
+            for (int i = 0; i <= n; i++) parent[i] = i;
+        }
+        
+        int findUPar(int node){
+            if (parent[node] == node) return node;
+            return parent[node] = findUPar(parent[node]);
+        }
+        
+        void unionByRank (int u, int v){
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+            
+            if (ulp_u  == ulp_v) return;
+            
+            if (rank[ulp_u] < rank[ulp_v]) parent[ulp_u] = ulp_v;
+            else if (rank[ulp_v] < rank[ulp_u]) parent[ulp_v] = ulp_u;
+            else {
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
+        }
+};
+
 class Solution {
+    static bool compare(vector<int> a, vector<int> b){
+        return a[2] < b[2];
+    }
   public:
     int spanningTree(int V, vector<vector<int>>& edges) {
-        vector<vector<pair<int, int>>> graph(V);
+        sort(edges.begin(), edges.end(), compare);
         
-        for (auto it : edges){
-            graph[it[0]].push_back({it[1], it[2]});
-            graph[it[1]].push_back({it[0], it[2]});
-        }
-        
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        vector<int> visited(V, 0);
-        
-        pq.push({0, 0, -1}); //wt, node, parent
         int res = 0;
         
-        while (!pq.empty()){
-            int wt = pq.top()[0];
-            int node = pq.top()[1];
-            int par = pq.top()[2];
-            
-            pq.pop();
-            
-            if (!visited[node]) res += wt;
-            
-            visited[node] = 1;
-            
-            for (auto adj : graph[node]){
-                if (!visited[adj.first]) pq.push({adj.second, adj.first});
-            }
-            
-        }
+        DisjointSet ds(V);
         
+        for (auto it : edges){
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            
+            if (ds.findUPar(u) != ds.findUPar(v)){
+                res += wt;
+                ds.unionByRank(u, v);
+            }
+        }
         return res;
     }
 };
