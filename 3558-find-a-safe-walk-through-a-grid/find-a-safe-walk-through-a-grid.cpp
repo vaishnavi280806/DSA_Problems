b10a91b17964e5c4cbf34cca4e376c14bb6e119f
Vaishnavi S <vaishnavi280806@gmail.com>
2026-07-02 22:54:25 +0530

    Time: 78 ms (20.96%) | Memory: 30.6 MB (75.65%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> dr = {-1, 0, 0, 1};
+        vector<int> dc = {0, -1, 1, 0};
+
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+        pq.push({health - grid[0][0], {0, 0}});
+        vector<vector<int>> dist(m, vector<int>(n, INT_MIN));
+        dist[0][0] = health - grid[0][0];
+
+        while (!pq.empty()) {
+            int h = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
+            pq.pop();
+
+            if (r == m - 1 && c == n - 1 && h >= 1)
+                return true;
+
+            for (int i = 0; i < 4; i++) {
+                int rr = r + dr[i];
+                int cc = c + dc[i];
+
+                if (rr < 0 || rr >= m || cc < 0 || cc >= n) continue;
+                if (h - grid[rr][cc] >= 1 && dist[rr][cc] < h - grid[rr][cc]) {
+                    dist[rr][cc] = h - grid[rr][cc];
+                    pq.push({h - grid[rr][cc], {rr, cc}});
+                }
+            }
+        }
+        
+        return false;
+    }
+};
