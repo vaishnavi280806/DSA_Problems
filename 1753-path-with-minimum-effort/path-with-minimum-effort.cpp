afedd469df2669e575f1336dbc1926aab1e31d8c
Vaishnavi S <vaishnavi280806@gmail.com>
2026-07-03 23:22:35 +0530

    Time: 51 ms (67.79%) | Memory: 24.4 MB (91.49%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -3,39 +3,41 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
+
         vector<int> dr = {-1, 0, 0, 1};
         vector<int> dc = {0, -1, 1, 0};
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        pq.push({0, {0, 0}});
 
         vector<vector<int>> dist(m, vector<int> (n, INT_MAX));
         dist[0][0] = 0;
 
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-
-        pq.push({0, {0, 0}});
-
         while (!pq.empty()){
             int diff = pq.top().first;
-            auto cur = pq.top().second;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
             pq.pop();
 
-            if (cur.first == m-1 && cur.second == n-1) return diff;
+            if (r == m-1 && c == n-1) return diff;
 
             for (int i = 0; i < 4; i++){
-                int rr = cur.first + dr[i];
-                int cc = cur.second + dc[i];
+                int rr = r + dr[i];
+                int cc = c + dc[i];
 
                 if (rr < 0 || rr >= m || cc < 0 || cc >= n) continue;
-                int newd = abs(heights[rr][cc] - heights[cur.first][cur.second]);
-                if (newd > diff && newd < dist[rr][cc]){
-                    dist[rr][cc] = newd;
+                int newd = abs(heights[rr][cc] - heights[r][c]);
+
+                if (diff < newd && newd < dist[rr][cc]){
                     pq.push({newd, {rr, cc}});
+                    dist[rr][cc] = newd;
                 }
-                else if (newd <= diff && diff < dist[rr][cc]){
-                    dist[rr][cc] = diff;
+                else if (diff >= newd && diff < dist[rr][cc]){
                     pq.push({diff, {rr, cc}});
+                    dist[rr][cc] = diff;
                 }
             }
         }
-        return -1;        
+        return -1;
     }
 };
