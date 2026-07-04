fce372e0a57212ba6088e7cbd1bebc57cbfd34d9
Vaishnavi S <vaishnavi280806@gmail.com>
2026-07-05 02:36:42 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (36.71%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx = -1;
+        //1 2 4 9 6 5 3 -> 1 2 5 3 4 6 9
+        //1 2 5 4 3 -> 1 3 2 4 5
         int n = nums.size();
+
+        //step 1: 1 2 5 4 3 -> find the idx where it decreases
+        int  idx = -1;
         for (int i = n-2; i >= 0; i--){
             if (nums[i] < nums[i+1]){
                 idx = i;
                 break;
             }
         }
-        if (idx == -1){
-            reverse(nums.begin(), nums.end());
-            return;
-        }
+
+        // it is in descending order
+        if (idx == -1) return reverse(nums.begin(), nums.end());
+
+        //step 2: 1 3 5 4 2 -> swap a smallest number > than nums[idx] at idx
         for (int i = n-1; i >= idx; i--){
             if (nums[i] > nums[idx]){
                 swap(nums[i], nums[idx]);
                 break;
             }
         }
+
+        //step 3: 1 3 2 4 5 -> simply reverse after idx
         reverse(nums.begin() + idx + 1, nums.end());
+
+
+
     }
 };
