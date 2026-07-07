e088f8632356d37fe6b38c317f5b41a9faa4659f
Vaishnavi S <vaishnavi280806@gmail.com>
2026-07-07 10:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (74.01%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -3,20 +3,19 @@ public:
     long long sumAndMultiply(int n) {
         #define ll long long
         ll sum = 0;
-        ll rev = 0;
         ll x = 0;
+        ll mul = 1;
 
         while (n){
             int r = (n % 10);
-            if (r != 0) rev = (rev * 10) + r;
+            sum += r;
+            if (r != 0){
+                x = r * mul + x;
+                mul *= 10;
+            }
             n /= 10;
         }
-        while (rev){
-            int r = rev % 10;
-            sum += r;
-            x = x * 10 + r;
-            rev /= 10;
-        }
+        
         //cout << x << " " << sum;
         return sum * x;
     }
