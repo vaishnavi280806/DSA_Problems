94337eed08a11c2f065875c02c1c38d71838029b
Vaishnavi S <vaishnavi280806@gmail.com>
2026-07-14 10:29:56 +0530

    Time: 55 ms (40.02%) | Memory: 21.1 MB (28.21%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
-    int helper(int i, int j, int k, vector<vector<int>>& grid, int m, int n, vector<vector<vector<int>>>& dp){
-        if (i < 0 || i >= m || j < 0 || j >= n || k < 0 || k >= n) return -1e5;
-        if (i == m-1){
-            if (j != k) return grid[m-1][j] + grid[m-1][k];
-            else return grid[m-1][j];
+    int helper(int i, int j, int k, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
+        if (i >= grid.size() || j < 0 || j >= grid[0].size() || k < 0 || k >= grid[0].size()) return -1e5;
+        if (i == grid.size() -1){
+            if (j != k) return grid[i][j] + grid[i][k];
+            return grid[i][j];
         }
         if (dp[i][j][k] != -1) return dp[i][j][k];
-        int maxi = -1e5;
-        int ans;
+
+        int maxi  = INT_MIN;
         for (int r1 = -1; r1 <= 1; r1++){
             for (int r2 = -1; r2 <= 1; r2++){
-                if (j != k) ans = grid[i][j] + grid[i][k] + helper(i+1, j + r1, k + r2, grid, m, n, dp);
-                else ans = grid[i][j] + helper(i+1, j + r1, k + r2, grid, m, n, dp);
-                maxi = max(maxi, ans);
+                if (j != k) maxi = max(maxi, grid[i][j] + grid[i][k] + helper(i+1, j + r1, k+r2, grid, dp));
+                else maxi = max(maxi, grid[i][j] + helper(i+1, j + r1, k+r2, grid, dp));
             }
         }
         return dp[i][j][k] = maxi;
@@ -21,7 +20,8 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<vector<int>>> dp (m, vector<vector<int>> (n, vector<int> (n, -1)));
-        return helper(0, 0, n-1, grid, m, n, dp);
+
+        vector<vector<vector<int>>> dp(m, vector<vector<int>> (n, vector<int> (n, -1)));
+        return helper(0, 0, n-1, grid, dp);
     }
 };
