d091b0352e4ba00b4ffb664fff563f1e20a290a4
Vaishnavi S <vaishnavi280806@gmail.com>
2026-07-14 09:17:33 +0530

    Time: 4 ms (15.17%) | Memory: 16.3 MB (61.86%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,49 +1,19 @@
 class Solution {
-    /*int helper(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
+    int helper(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
         if (i == 0 && j == 0) return grid[0][0];
         if (i < 0 || j < 0) return 1e5;
         if (dp[i][j] != -1) return dp[i][j];
+
         int up = grid[i][j] + helper(i-1, j, grid, dp);
         int left = grid[i][j] + helper(i, j-1, grid, dp);
 
         return dp[i][j] = min(up, left);
-    }*/
+    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        //vector<vector<int>> dp(m, vector<int> (n, -1));
-        //return helper(m-1, n-1, grid, dp);
-       
-        /*for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (i == 0 && j == 0) dp[0][0] = grid[0][0];
-                else{
-                    int up = INT_MAX, left = INT_MAX;
-                    if (i-1 >= 0) up = grid[i][j] + dp[i-1][j];
-                    if (j -1 >= 0) left = grid[i][j] + dp[i][j-1];
-
-                    dp[i][j] = min(up, left);
-                }
-            }
-        }
-        return dp[m-1][n-1]; */
-
-        vector<int> prev(n, 1e5);
-        for (int i = 0; i < m; i++){
-            vector<int> temp(n);
-            for (int j = 0; j < n; j++){
-                if (i == 0 && j == 0) temp[0] = grid[0][0];
-                else{
-                    int up = INT_MAX, left = INT_MAX;
-                    if (i-1 >= 0) up = grid[i][j] + prev[j];
-                    if (j -1 >= 0) left = grid[i][j] + temp[j-1];
-
-                    temp[j] = min(up, left);
-                }
-            }
-            prev = temp;
-        }
-        return prev[n-1];
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return helper(m-1, n-1, grid, dp);
     }
 };
