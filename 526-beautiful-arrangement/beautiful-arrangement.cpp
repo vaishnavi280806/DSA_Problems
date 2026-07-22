61501a97b46fc9dcfd6a18f0cda2e211b9eb05e7
Vaishnavi S <vaishnavi280806@gmail.com>
2026-07-23 00:33:22 +0530

    Time: 5 ms (78.83%) | Memory: 18.7 MB (12.68%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -1,15 +1,17 @@
 class Solution {
-    int helper(int pos, int mask, int n){
+    int helper(int pos, int mask, int n, vector<vector<int>>& dp){
         if(pos == n+1) return 1;
+        if (dp[pos][mask] != -1) return dp[pos][mask];
 
         int res = 0;
         for(int i = 1; i <= n; i++){
-            if ((mask & (1 << i)) == 0) if(i % pos == 0 || pos % i == 0) res += helper(pos+1, mask | (1 << i), n);
+            if ((mask & (1 << i)) == 0) if(i % pos == 0 || pos % i == 0) res += helper(pos+1, mask | (1 << i), n, dp);
         }
-        return res;
+        return dp[pos][mask]= res;
     }
 public:
     int countArrangement(int n) {
-        return helper(1, 0, n);
+        vector<vector<int>> dp(n+1, vector<int> (1 << (n+1), -1));
+        return helper(1, 0, n, dp);
     }
 };
