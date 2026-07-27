d481c5bd5e3985b26c255af643b2c3394e3a6009
Vaishnavi S <vaishnavi280806@gmail.com>
2026-07-27 19:03:23 +0530

    Time: 15 ms (60.05%) | Memory: 46.7 MB (43.63%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,56 +1,51 @@
-class DisjointSet {
-    public:
-        vector<int> rank, parent;
+class DSU{
+public:
+    vector<int> parent, rank;
+    DSU (int n){
+        rank.resize(n, 1);
+        parent.resize(n);
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
 
-        DisjointSet(int n){
-            rank.resize(n, 0);
-            parent.resize(n);
-            for (int i = 0; i <n; i++) parent[i] = i;
+    int find (int node){
+        if (node == parent[node]) return node;
+        else return parent[node] = find(parent[node]);
+    }
+
+    void unionR(int u, int v){
+        int ulp_u = find(u);
+        int ulp_v = find(v);
+
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) parent[ulp_u] = ulp_v;
+        else if (rank[ulp_u] > rank[ulp_v]) parent[ulp_v] = ulp_u;
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
         }
-
-        int findUPar(int node){
-            if (node == parent[node]) return node;
-            return parent[node] = findUPar(parent[node]);
-        }
-
-        void unionR(int u, int v){
-            int ulp_u = findUPar(u);
-            int ulp_v = findUPar(v);
-
-            if (ulp_u == ulp_v) return;
-            if (rank[ulp_u] < rank[ulp_v]) parent[ulp_u] = ulp_v;
-            else if (rank[ulp_v] < rank[ulp_u]) parent[ulp_v] = ulp_u;
-            else{
-                rank[ulp_u]++;
-                parent[ulp_v] = ulp_u;
-            }
-           
-        }
+    }
 };
 
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        DisjointSet ds(n);
+        DSU dsu(n);
         int extra = 0;
+
         for (auto it : connections){
             int u = it[0];
             int v = it[1];
 
-            if (ds.findUPar(u) == ds.findUPar(v)){
-                extra++;
-            }
-            else{
-                ds.unionR(u, v);
-            }
+            if (dsu.find(u) == dsu.find(v)) extra++;
+            else dsu.unionR(u, v);
         }
 
         int comp = 0;
         for (int i = 0; i < n; i++){
-            if (ds.findUPar(i) == i) comp++;
+            if (dsu.find(i) == i) comp++;
         }
 
-        if (extra >= comp - 1) return comp-1;
+        if (extra >= comp-1) return comp-1;
         return -1;
     }
 };
